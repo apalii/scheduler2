0a66c74acdca863019dc87308c56b20bac4ea64b
Andrii Palii <Andrii.Palii@portaone.com>
2016-02-22 17:57:10 +0200

    heroku fix part 2

--- django_test/settings.py
+++ django_test/settings.py
@@ -1,13 +1,17 @@
 import os
 
-PRODUCTION = False
+try:
+    PRODUCTION = os.environ['PRODUCTION']
+except KeyError:
+    PRODUCTION = False
+ 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DEBUG = True if PRODUCTION == False else False
 TEMPLATE_DEBUG = DEBUG
 
 if PRODUCTION:
-    ALLOWED_HOSTS = ['task.portaone.com']
+    ALLOWED_HOSTS = ['task.portaone.com', 'murmuring-lake-98065.herokuapp.com']
 
 # Tastypie settings
 API_LIMIT_PER_PAGE = 15
@@ -67,20 +71,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    #'/Users/jmitch/desktop/lwc/src/static/static_dirs/',
-    # '/Users/jmitch/desktop/lwc/src/static/static_dirs/',
-    # '/Users/jmitch/desktop/lwc/src/static/static_dirs/',
-    # '/Users/jmitch/desktop/lwc/src/static/static_dirs/',
-)
 
 # List of finder classes that know how to find static files in
 # various locations.
